a52497d9283b419159422908ac5124342f698444
JorgimDoKuruma <j.santosyamato@gmail.com>
2025-05-07 22:26:17 -0300

    WIP: implement configuration file

--- hardlinker.py
+++ hardlinker.py
@@ -1,10 +1,16 @@
 import sqlalchemy
 from sqlalchemy.orm import declarative_base, sessionmaker
 import qbittorrentapi
+import configparser
 import os
 
-torrent_category = ""
-destination_path = ""
+# import configurations
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+torrent_category = config["DEFAULT"]["torrent_category"]
+destination_path = config["DEFAULT"]["destination_path"]
 
 # DB connection
 
@@ -32,10 +38,10 @@ session = Sesssion()
 # QBIT connection
 
 qbt_conn_info = dict(
-    host="0",
-    port=0,
-    username="admin",
-    password="heheboy",
+    host=config["DEFAULT"]["qbt_host"],
+    port=config["DEFAULT"]["qbt_port"],
+    username=config["DEFAULT"]["qbt_username"],
+    password=config["DEFAULT"]["qbt_password"],
 )
 
 qbt_client = qbittorrentapi.Client(**qbt_conn_info)
@@ -73,23 +79,23 @@ for torrent in torrents:
         
     # Check if the amount left is 0
     # Check also if the status is uploading or pausedUP or queuedUP or stalledUP
-    if torrent.amount_left == 0 and torrent.state in ["uploading", "pausedUP", "queuedUP", "stalledUP"]:
-        # Check if the torrent is already linked
-        if not db_torrent.linked:
-            # Get the files of the torrent
-            files = qbt_client.torrents_files(torrent.hash)
-            for file in files:
-                # Check if the file is a .mkv file
-                if file.name.endswith(".mkv"):
-                    # Create the destination path
-                    dest_path = os.path.join(destination_path, file.name)
-                    # Create a hardlink to the file
-                    qbt_client.torrents_hardlink(torrent.hash, dest_path)
-                    print(f"Created hardlink for {file.name} in {dest_path}.")
-            db_torrent.linked = True
-            session.commit()
-            print(f"Linked torrent {torrent.name}.")
-        else:
-            print(f"Torrent {torrent.name} is already linked.")
-    else:
-        print(f"Torrent {torrent.name} is not completed yet.")
+    # if torrent.amount_left == 0 and torrent.state in ["uploading", "pausedUP", "queuedUP", "stalledUP"]:
+    #     # Check if the torrent is already linked
+    #     if not db_torrent.linked:
+    #         # Get the files of the torrent
+    #         files = qbt_client.torrents_files(torrent.hash)
+    #         for file in files:
+    #             # Check if the file is a .mkv file
+    #             if file.name.endswith(".mkv"):
+    #                 # Create the destination path
+    #                 dest_path = os.path.join(destination_path, file.name)
+    #                 # Create a hardlink to the file
+    #                 qbt_client.torrents_hardlink(torrent.hash, dest_path)
+    #                 print(f"Created hardlink for {file.name} in {dest_path}.")
+    #         db_torrent.linked = True
+    #         session.commit()
+    #         print(f"Linked torrent {torrent.name}.")
+    #     else:
+    #         print(f"Torrent {torrent.name} is already linked.")
+    # else:
+    #     print(f"Torrent {torrent.name} is not completed yet.")
